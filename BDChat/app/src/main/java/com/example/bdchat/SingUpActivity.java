759815efddedde32217534ffb9e0e220da748151
Sarifin007 <s.arifinoriginal@gmail.com>
2023-02-27 00:01:01 +0600

    aa
    aa

---
+++ BDChat/app/src/main/java/com/example/bdchat/SingUpActivity.java
@@ -0,0 +1,16 @@
+package com.example.bdchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up);
+        getSupportActionBar().hide();
+
+    }
+}
