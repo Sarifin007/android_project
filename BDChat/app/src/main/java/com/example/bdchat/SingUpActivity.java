319346854b8eda8669459c1cd11b4746cb59210f
Sarifin007 <99475443+Sarifin007@users.noreply.github.com>
2023-02-28 11:59:57 +0600

    singin
    singin

--- BDChat/app/src/main/java/com/example/bdchat/SingUpActivity.java
+++ BDChat/app/src/main/java/com/example/bdchat/SingUpActivity.java
@@ -4,12 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.bdchat.databinding.ActivitySingUpBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class SingUpActivity extends AppCompatActivity {
+
+    ActivitySingUpBinding singUpBinding;
+    private FirebaseAuth auth;
+    FirebaseDatabase firebaseDatabase;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sing_up);
+      singUpBinding=ActivitySingUpBinding.inflate(getLayoutInflater());
+      setContentView(singUpBinding.getRoot());
+      auth=FirebaseAuth.getInstance();
+
+
         getSupportActionBar().hide();
 
     }
