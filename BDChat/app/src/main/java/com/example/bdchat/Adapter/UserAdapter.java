f1c62fe4aa7024e5f5694c6397a61033469088f4
Sarifin007 <99475443+Sarifin007@users.noreply.github.com>
2023-03-02 12:29:46 +0600

    ww
    ww

---
+++ BDChat/app/src/main/java/com/example/bdchat/Adapter/UserAdapter.java
@@ -0,0 +1,64 @@
+package com.example.bdchat.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.bdchat.Models.User;
+import com.example.bdchat.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+
+    ArrayList<User> list;
+    Context context;
+
+    public UserAdapter(ArrayList<User> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       View view= LayoutInflater.from(context).inflate(R.layout.sample_show_user,parent,false);
+       return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserAdapter.ViewHolder holder, int position) {
+        User users=list.get(position);
+        Picasso.get().load(users.getProfilePic()).placeholder(R.drawable.avatar3).into(holder.image);
+        holder.userName.setText(users.getUserName());
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public  class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView image;
+        TextView userName,lastMessage;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            image=itemView.findViewById(R.id.profile_image);
+            userName=itemView.findViewById(R.id.userNameList);
+            lastMessage=itemView.findViewById(R.id.lastMassage);
+
+        }
+    }
+}
